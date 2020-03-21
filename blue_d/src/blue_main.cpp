9195d47c6399473e214898a3851b8cb2808904b8
brandon phelps <bwp44f@mst.edu>
2020-03-21 16:29:51 -0500

    attemp at circle drawing

--- blue_d/src/blue_main.cpp
+++ blue_d/src/blue_main.cpp
@@ -16,6 +16,40 @@ uint8_t pos = 3;
 const uint16_t MapXOffset = 10;
 const uint16_t MapYOffset = 10;
 
+class Point
+{
+public:
+	uint8_t x_pos;
+	uint8_t y_pos;
+};
+
+class Map
+{
+public:
+	// 10 by 10 grid
+	uint8_t tile_info[100];
+	uint8_t width;
+	uint8_t height;
+
+
+
+};
+
+const uint8_t currentmap[10 * 10] = {
+                                     1, 1, 1, 1, 1,  1, 1, 1, 1, 1, 
+                                     1, 0, 0, 0, 0,  0, 0, 1, 1, 1, 
+                                     1, 0, 1, 0, 0,  0, 0, 0, 0, 1, 
+                                     1, 0, 1, 0, 0,  0, 0, 1, 1, 1, 
+                                     1, 0, 1, 0, 0,  1, 0, 1, 1, 1, 
+                                     
+                                     1, 0, 1, 0, 0,  0, 0, 1, 1, 1, 
+                                     1, 0, 1, 1, 1,  0, 0, 0, 0, 1, 
+                                     1, 0, 1, 0, 0,  0, 0, 1, 1, 1, 
+                                     1, 0, 0, 0, 0,  0, 0, 0, 0, 1, 
+                                     1, 0, 0, 0, 0,  0, 0, 1, 1, 1, };
+
+
+
 void DrawMap(uint8_t* buffer, uint32_t buf_width, uint32_t buf_height, uint8_t* tiles)
 {
 	// 
@@ -41,29 +75,75 @@ void DrawMap(uint8_t* buffer, uint32_t buf_width, uint32_t buf_height, uint8_t* 
 			}
 		}
 	}
-
-	// DrawRectangle(buffer, buf_width, buf_height,
-	//               20 + 0 * 10, 20 + 0 * 10,
-	//               10, 10, 0xFF, 0x00, 0xFF);
-
-	// DrawRectangle(buffer, buf_width, buf_height,
-	//               1, pos,
-	//               10, 10, 0xFF, 0x00, 0xFF);
+}
 
 
-	// {
-	// 	uint32_t offset = get_2d_offset(0, row, 10, 10);
-	// 	if(tiles[offset] == 1)
-	// 	{
-	// 		DrawRectangle(buffer, buf_width, buf_height, row*TileHeight, 0*TileWidth,
-	// 		              TileWidth-10, TileHeight-10, 0xFF, 0x00, 0x00);
-	// 	}
-	// 	else
-	// 	{
-	// 		DrawRectangle(buffer, buf_width, buf_height, row*TileHeight, 0*TileWidth,
-	// 		              TileWidth-10, TileHeight-10, 0x00, 0xFF, 0x00);
-	// 	}
-	// }
+void draw_circle_helper(ScreenData* screenData,
+                        uint32_t center_x, uint32_t center_y,
+                        uint32_t x, uint32_t y)
+{
+	uint32_t pixelIndex;
+	uint32_t* pixel;
+	pixelIndex = (center_x + x) + ((y + center_y) * screenData->width);
+	if(pixelIndex > (screenData->width * screenData->height))
+	{
+		return;
+	}
+	pixel = (uint32_t*)(screenData->buffer + pixelIndex);
+	*pixel = 0x0000FFFF;
+
+	pixelIndex = (center_x - x) + ((y + center_y) * screenData->width);
+	if(pixelIndex > (screenData->width * screenData->height))
+	{
+		return;
+	}
+	pixel = (uint32_t*)(screenData->buffer + pixelIndex);
+	*pixel = 0x0000FFFF;
+}
+
+void DrawCircle(ScreenData* screenData,
+                uint32_t center_x, uint32_t center_y,
+                uint32_t r)
+{
+	uint32_t x = 0;
+	uint32_t y = r;
+	uint32_t d = 3 - y * r;
+	draw_circle_helper(screenData, center_x, center_y, x, y);
+	while(y >= x)
+	{
+		x++;
+		if(d > 0)
+		{
+			y--;
+			d = d + 4 * (x - y) + 10;
+		}
+		else
+		{
+			d = d + 4 * x + 6;
+		}
+		draw_circle_helper(screenData, center_x, center_y, x, y);
+	}
+}
+
+void DrawCircle(ScreenData* screenData,
+                uint32_t center_x, uint32_t center_y,
+                uint32_t r, bool t)
+{
+	// draw_circle_helper(screenData, center_x, center_y, x, y);
+
+	// draw_circle_helper(screenData, center_x, center_y, 0, r);
+
+	uint32_t x = center_x;
+	uint32_t y = center_y;
+	
+	uint32_t pixelIndex = x + (y  * screenData->width);
+	uint32_t* pixel = (uint32_t*)(screenData->buffer + pixelIndex);
+	*pixel = 0xFF00FFFF;
+}
+
+void DrawToon(ScreenData* screenData, Point* toon)
+{
+	DrawCircle(screenData, toon->x_pos, toon->y_pos, 20, true);
 }
 
 void blueDraw(uint8_t* buffer, uint32_t buf_width, uint32_t buf_height, uint32_t pitch)
@@ -119,37 +199,6 @@ void blueDraw(uint8_t* buffer, uint32_t buf_width, uint32_t buf_height, uint32_t
 
 }
 
-class Point
-{
-public:
-	uint8_t x_pos;
-	uint8_t y_pos;
-};
-
-class Map
-{
-public:
-	// 10 by 10 grid
-	uint8_t tile_info[100];
-	uint8_t width;
-	uint8_t height;
-
-
-
-};
-
-const uint8_t currentmap[10 * 10] = {
-                                     1, 1, 1, 1, 1,  1, 1, 1, 1, 1, 
-                                     1, 0, 0, 0, 0,  0, 0, 1, 1, 1, 
-                                     1, 0, 1, 0, 0,  0, 0, 0, 0, 1, 
-                                     1, 0, 1, 0, 0,  0, 0, 1, 1, 1, 
-                                     1, 0, 1, 0, 0,  1, 0, 1, 1, 1, 
-                                     
-                                     1, 0, 1, 0, 0,  0, 0, 1, 1, 1, 
-                                     1, 0, 1, 1, 1,  0, 0, 0, 0, 1, 
-                                     1, 0, 1, 0, 0,  0, 0, 1, 1, 1, 
-                                     1, 0, 0, 0, 0,  0, 0, 0, 0, 1, 
-                                     1, 0, 0, 0, 0,  0, 0, 1, 1, 1, };
 
 // works fine on windows, but something about console doesn't allow writing to.
 extern "C" int GameInit(GameState* game_state)
@@ -187,19 +236,35 @@ extern "C" int GameUpdate(int dt, ScreenData* screenData, GameState* game_state,
 
 	if(controller != NULL)
 	{
+		if(controller->MoveUp.EndedDown)
+		{
+			toon->y_pos--;
+		}
+
+		if(controller->MoveDown.EndedDown)
+		{
+			toon->y_pos++;
+		}
+
 		if(controller->MoveRight.EndedDown)
 		{
-			pos++;
+			toon->x_pos++;
 		}
 		if(controller->MoveLeft.EndedDown)
 		{
-			pos--;
+			toon->x_pos--;
 		}
 	}
 
+	if(toon->x_pos < 0)
+	{
+		toon->x_pos = 0;
+	}
 
+	toon->y_pos = 10;
+	
 	DrawMap(screenData->buffer, screenData->width, screenData->height, p->tile_info);
-
+	DrawToon(screenData, toon);
 
 	return 0;
 }
