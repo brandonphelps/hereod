d8a6fb3d05dbf3783fbce65d3e275070f0f74b12
brandon phelps <bwp44f@mst.edu>
2020-04-25 20:01:22 -0500

    a bit more flushing of console with buffer history

--- windows/src/main.cpp
+++ windows/src/main.cpp
@@ -33,7 +33,6 @@
 
 
 #include "clanger.h"
-#include "subprocess.hpp"
 
 void my_clanger_printer(const std::string& msg)
 {
@@ -194,6 +193,8 @@ public:
 	// render_dest is the location of where the console should be rendered onto. 
 	void render(ScreenData& render_dest);
 
+	void renderString(ScreenData& window, const std::string& msg, int start_x, int start_y);
+
 	// note this does not allow for multiple  key pressed at the same time.
 	// update to comput a keybord or game controller input. 
 	void update(uint32_t keyCode);
@@ -210,12 +211,34 @@ public:
 void Console::update(uint32_t keycode)
 {
 	WriteLine("Updating message with keycode: " + std::to_string(keycode));
-	current_message += static_cast<char>(keycode);
-	WriteLine("Current message: " +current_message);
+
+
 	if(keycode == VK_RETURN)
 	{
+		buffer_history.push_back(current_message);
 		current_message = "";
 	}
+	else if(keycode == VK_BACK)
+	{
+
+		if(!current_message.empty())
+		{
+			int prev_size = current_message.size();
+			WriteLine("BACK Space");
+			current_message.pop_back();
+			int after_size = current_message.size();
+			if(prev_size != after_size)
+			{
+				WriteLine("pop back is a lie: " + std::to_string(prev_size) + ", " + std::to_string(after_size));
+			}
+		}
+	}
+	else
+	{
+		current_message += static_cast<char>(keycode);
+	}
+		
+	WriteLine("Current message: " + current_message);
 	// // if keycode is a normal key value A-Z0-9, add to current active string
 	// // if keycode is a enter key add string to buffer_history.
 	// if(console_display_keys.find(keycode) != console_display_keys.end())
@@ -228,34 +251,46 @@ void Console::update(uint32_t keycode)
 	// }
 }
 
-void Console::render(ScreenData& render_dest)
+void Console::renderString(ScreenData& dest, const std::string& str, int start_x, int start_y)
 {
-
-	DrawRectangle(render_window.buffer, render_window.width, render_window.height, 0, 0, render_window.width, render_window.height, 0x00, 0x00, 0x00);
-
 	// update the render window as needed.
 	int index = 0;
-	for(int i = 0; i < current_message.size(); i++)
+	for(int i = 0; i < str.size(); i++)
 	{
-		if(current_message[i] == 'A' || current_message[i] == 'a')
+		if(str[i] >= 48 && str[i] <= 57)
 		{
-			index = 0;
+			index = (str[i] - 48) + 26;
 		}
-		else if(current_message[i] == 'B' || current_message[i] == 'b')
+		else if(str[i] >= 65 && str[i] <= 90)
 		{
-			index = 1;
-		}
-		else if(current_message[i] == 'W' || current_message[i] == 'w')
-		{
-			index = 2;
+			index = toupper(str[i]) - 65;
 		}
 		else
 		{
 			continue;
 		}
 
-		BlitScreenData(font_sheet.GetSprite(index), render_window, i * font_sheet.sprite_width, 0);
+		BlitScreenData(font_sheet.GetSprite(index), dest,
+		               start_x + (i * font_sheet.sprite_width), start_y);
 	}
+}
+
+void Console::render(ScreenData& render_dest)
+{
+
+	DrawRectangle(render_window.buffer, render_window.width, render_window.height, 0, 0, render_window.width, render_window.height, 0x00, 0x00, 0x00);
+
+
+	int high_index = 2;
+	for(int i = buffer_history.size()-1; i >= 0; --i)
+	{
+		renderString(render_window, buffer_history[i],
+		             0, render_window.height - (high_index++ * font_sheet.sprite_height));
+	}
+
+	renderString(render_window, current_message,
+	             0, render_window.height - font_sheet.sprite_height);
+
 
 	BlitScreenData(render_window, render_dest, 200, 200);
 }
@@ -392,13 +427,13 @@ LRESULT CALLBACK WndProc2(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 					StartAddress = reinterpret_cast<uintptr_t>(StartMemPrint);
 					std::string Address = toHex(StartAddress);
-					TextOut(hdc, 40, 40, Address.c_str(), Address.size());
+					TextOutA(hdc, 40, 40, Address.c_str(), Address.size());
 					static uint8_t mem_print_width = 50;
 					for(int i = 0; i < 13; i++)
 					{
 						uint8_t* platname = StartMemPrint + (i * mem_print_width);
 						std::string valueStr = toHex(platname, mem_print_width);
-						TextOut(hdc, 40, 65 + (15 * i), valueStr.c_str(), valueStr.size());
+						TextOutA(hdc, 40, 65 + (15 * i), valueStr.c_str(), valueStr.size());
 					}
 				
 					SelectObject(hdc, hOldFont);
